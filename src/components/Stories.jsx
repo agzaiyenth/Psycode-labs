8c4f26bf7afcab1f2907e5f240f6ef78fbb5defe
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2024-09-03 10:17:30 +0530

    edit video component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -4,6 +4,7 @@ import {
 import Heading from "./Heading";
 import Section from "./Section";
 import Testimonials from "./Testimonials";
+import VideoDialog from "./video";
 
 const Stories = () => {
   return (
@@ -15,15 +16,24 @@ const Stories = () => {
         />
 
         <div className="relative">
-          <div className="relative z-1 flex flex-col items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-          <iframe
+          <div className="relative z-1 flex flex-col items-center  mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 ">
+          {/* <iframe
                 className="w-full h-full object-cover md:object-right"
                 width={1500}
                 alt="Testimonial"
                 height={730}
                 src="https://www.youtube.com/embed/VqFfOGJuBQA?autoplay=1&fs=0&modestbranding=1">
 
-                </iframe>
+                </iframe> */}
+
+      <VideoDialog
+        className="hidden dark:block w-full h-full object-cover md:object-right"
+        animationStyle="top-in-bottom-out"
+        videoSrc="https://www.youtube.com/embed/VqFfOGJuBQA?autoplay=1&fs=0&modestbranding=1"
+        thumbnailSrc="https://placehold.co/1920x1080"
+        thumbnailAlt="Video"
+      />
+    
                 
                
         
