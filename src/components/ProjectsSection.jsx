910dc1d4c8be4c1b2d581be0030fc971e94c2e97
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-09-04 14:11:14 +0530

    sum

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -39,27 +39,7 @@ const Projects = () => {
         <div className="pb-20 xl:pb-[130px]">
           <div className="global-container">
             <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 lg:gap-20 xl:grid-cols-[minmax(0,_1fr)_.8fr] xl:gap-28 xxl:gap-[134px]">
-              <div
-                className="jos"
-                data-jos_once="1"
-                data-jos_animation="fade-up"
-                data-jos_timingfunction="ease"
-                data-jos_duration="0.7"
-                data-jos_delay="0.5"
-                data-jos_counter="1"
-              >
-                <div className="mb-6">
-                <Heading className="md:max-w-md lg:max-w-2xl" title="Trusted by Clients"/>
-            
-                </div>
-                <p className="mb-7 last:mb-0 body-1  text-n-2 ">
-                  At Psycode Labs, we don&apos;t just build platforms; we create
-                  experiences that empower and uplift. Trusted by clients across
-                  industries, our dedication to excellence is reflected in every
-                  line of code, every feature we craft, and every satisfied
-                  customer we serve.
-                </p>
-              </div>
+              
               <div className="flex flex-col gap-6 overflow-hidden rounded-[30px] bg-gradient-to-t from-[rgba(255,255,255,.1)] to-[rgba(0,0,0,0.5)] py-[10px]">
                 <Marquee
         gradient={false} 
@@ -81,6 +61,27 @@ const Projects = () => {
                   ))}
                 </div>
                 </Marquee>
+              </div>
+              <div
+                className="jos"
+                data-jos_once="1"
+                data-jos_animation="fade-up"
+                data-jos_timingfunction="ease"
+                data-jos_duration="0.7"
+                data-jos_delay="0.5"
+                data-jos_counter="1"
+              >
+                <div className="mb-6">
+                <Heading className="md:max-w-md lg:max-w-2xl" title="Trusted by Clients"/>
+            
+                </div>
+                <p className="mb-7 last:mb-0 body-1  text-n-2 ">
+                  We don&apos;t just build platforms; we create
+                  experiences that empower and uplift. Trusted by clients across
+                  industries, our dedication to excellence is reflected in every
+                  line of code, every feature we craft, and every satisfied
+                  customer we serve.
+                </p>
               </div>
             </div>
           </div>
