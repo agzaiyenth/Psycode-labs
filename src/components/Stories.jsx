0b091ee6fe359ac3c20d4657f762499f832d259b
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2024-09-12 20:58:24 +0530

    update utube

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -30,7 +30,7 @@ const Stories = () => {
       <VideoDialog
         className="hidden dark:block w-full h-full object-cover md:object-right"
         animationStyle="top-in-bottom-out"
-        videoSrc="https://youtu.be/ycUwjqcRj-A"
+        videoSrc="https://www.youtube.com/embed/ycUwjqcRj-A?autoplay=1&fs=0&modestbranding=1"
         thumbnailSrc="https://placehold.co/1920x1080"
         thumbnailAlt="Video"
       />
